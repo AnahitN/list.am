0d3ce38bdb20e2b4c7d5117a623615c1d0b8eab3
AnahitN <tanahit360@gmail.com>
2023-05-14 01:46:30 +0400

    List.am

---
+++ src/main/java/com/example/listam/entity/Comment.java
@@ -0,0 +1,23 @@
+package com.example.listam.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "comment")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne
+    private Item item;
+    private Date commentDate;
+    @Column(name = "comment")
+    private String commentText;
+
+
+
+}
